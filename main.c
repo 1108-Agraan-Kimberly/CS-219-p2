7258c10a8353e787f164f6fb468142a0a6199e08
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2024-10-20 06:28:52 +0000

    fscanf

--- main.c
+++ main.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-//#define txt "Programmin"
+#define txt "Programming-Project-2.txt"
 
 int main(){
+    FILE* commands;
+    commands = fopen("txt", "r");
+    if(commands = NULL){
+        printf("empty");
+        return 0;
+    }
+    char op[10];
+    uint32_t hexA, hexB;
+
+    while(fscanf(txt, "%s %")
  printf("Meow");
 
 }
