72bf866f6df33b4666ec369759a5776d3732c58c
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2024-10-29 23:26:57 +0000

    new

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Kimberly Agraan CS219 Project2
 ## How to Use
-Compile using ./gcc main.c -o main
+Compile using ./gcc operator.c -o start
 
-To run the code use ./main
+To run the code use ./start
 
 ## More Machine Operations
+From the last project, the program simulated how a machine does an add operation. In this program it added 8 of the other operations. The beginning will read for the operations and compare them to each defined command. If it matches the defined commands it'll perform the operations. The additional operations include: 
+
+**ADD** operation reads the two hex values and adds the second value to the first value in its binary conversion but returns the hex value of the sum.
+
+**AND** operation, this operation will read the hex values in the given txt file and it will compare the operands using the bitwise AND by converting the hex numbers to binary, check if both bits are 1 or not, if not the result would be a 0. 
+
+**ASR** operation is the arithmetic shift right operation where the first value in the txt file will be a hex value then a value of how many places to shift the first value. Since it is arithmetic it will keep the msb and shift everything to the right.
+
+**LSR** and **LSL** operations are logical shifiting operations where the msb gets shifted, in right shfiting the msb becomes 0 but in the left shift lsb becomes 0. 
+
+**NOT** operation the value in the file will be turned into a binary number and each bit will be inverted, the returned result will be the hex value of all the inverted bits. 
+ 
+**OR** operation will read the hex values, use the binary converions and compare each bit to each other, if the bits are both 0 then it will be 0, otherwise the result is a 1.
+ 
+**SUB** operation will read the two hex values and take the binary conversion and subtract the second operand value from the first one.
+
+**XOR** operation will read the hex values, use the binary converions and compare each bit to each other, if the bits are both 0 or 1 then it will be 0, otherwise the result is a 1.
+
+
+## Updating Flags
+ But there are more than the add operation, 8 other operations are also used along with the variation of that operation changing the status flag of the result. Operations with an S at the end make sure the instruction will update the flag if needed. The "S" in an opcode means the operation will perform as usual but also update the status flags based on the result. 
--- operator.c
+++ operator.c
@@ -1,3 +1,5 @@
+//Author: Kimberly Agraan
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,7 +16,6 @@ int main(){
     }
     char op[10];
     int shift;
-    uint32_t hexA, hexB;
 
     while(fscanf(commands,"%s", op) == 1){
         const char add[] = "ADD";
@@ -27,57 +28,66 @@ int main(){
         const char orr[] = "ORR";
         const char xor[] = "XOR";
         if(strcmp(op, add) == 0 || strstr(op, add)){
+            int32_t hexA, hexB;
             if(fscanf(commands, "%X %X", &hexA, &hexB) == 2){ //ADD operation
             int32_t addSum = hexA + hexB;
-            printf("%s 0x%X 0x%X < 0x%X > \n", op, hexA, hexB, addSum);
+            printf("%s 0x%X 0x%X: 0x%X  \n", op, hexA, hexB, addSum);
             } 
         } 
         else if(strcmp(op, and) == 0 || strstr(op, and)){
+            uint32_t hexA, hexB;
             if(fscanf(commands, "%X %X", &hexA, &hexB) == 2){ //AND operation
             uint32_t andSum = hexA & hexB;
-            printf("%s 0x%X 0x%X < 0x%X > \n", op, hexA, hexB, andSum);
+            printf("%s 0x%X 0x%X: 0x%X  \n", op, hexA, hexB, andSum);
             }             
         }        
         else if(strcmp(op, asr) == 0 || strstr(op, asr)){
+            int32_t hexA;
             if(fscanf(commands, "%X %d", &hexA, &shift) == 2){ //ASR operation
-                if(shift > 2){
-                int32_t right = hexA >> shift;  
-                printf("%s 0x%X %d : 0x%X  \n", op, hexA, shift, right);
-                }
-                else{
-                int32_t right = hexA / 2;
-                printf("%s 0x%X %d : 0x%X  \n", op, hexA, shift, right);
-                }
+                int32_t right = (int32_t) hexA >> shift;  
+                printf("%s 0x%X %d: 0x%X  \n", op, hexA, shift, right);
             }
-        }      
+        }
+        else if(strcmp(op, lsr) == 0 || strstr(op, lsr)){
+            uint32_t hexA;
+            if(fscanf(commands, "%X %d", &hexA, &shift) == 2){ //LSR operation
+                uint32_t right = hexA >> shift;  
+                printf("%s 0x%X %d: 0x%X  \n", op, hexA, shift, right);
+            }
+        }       
         else if(strcmp(op, lsl) == 0 || strstr(op, lsl)){
+            uint32_t hexA;
             if(fscanf(commands, "%X %d", &hexA, &shift) == 2){ //LSL operation
-                if(shift > 2){
-                int32_t left = hexA << shift;  
-                printf("%s 0x%X %d : 0x%X  \n", op, hexA, shift, left);
-                }
-                else{
-                int32_t left = hexA / 2;
-                printf("%s 0x%X %d : 0x%X  \n", op, hexA, shift, left);
-                }
+                uint32_t left = hexA << shift;  
+                printf("%s 0x%X %d: 0x%X  \n", op, hexA, shift, left);
             }
-        }     
+        }    
+        else if(strcmp(op, not) == 0 || strstr(op, not)){
+            uint32_t hexA;
+            if(fscanf(commands, "%X", &hexA) == 1){ //NOT operation
+            uint32_t no = ~hexA;
+            printf("%s 0x%X: 0x%X  \n", op, hexA, no);
+            }             
+        }  
         else if(strcmp(op, sub) == 0 || strstr(op, sub)){
+            uint32_t hexA, hexB;
             if(fscanf(commands, "%X %X", &hexA, &hexB) == 2){ //SUB operation
             uint32_t subSum = hexA - hexB;
-            printf("%s 0x%X 0x%X < 0x%X > \n", op, hexA, hexB, subSum);
+            printf("%s 0x%X 0x%X: 0x%X  \n", op, hexA, hexB, subSum);
             }            
         }
         else if(strcmp(op, orr) == 0 || strstr(op, orr)){
+            uint32_t hexA, hexB;
         if(fscanf(commands, "%X %X", &hexA, &hexB) == 2){ //ORR operation
                 uint32_t your = hexA | hexB;
-                printf("%s 0x%X 0x%X < 0x%X > \n", op, hexA, hexB, your);            
+                printf("%s 0x%X 0x%X: 0x%X  \n", op, hexA, hexB, your);            
             }            
         }
         else if(strcmp(op, xor) == 0 || strstr(op, xor)){
+            uint32_t hexA, hexB;
             if(fscanf(commands, "%X %X", &hexA, &hexB) == 2){ //XOR operation
                 uint32_t naur = hexA ^ hexB;
-                printf("%s 0x%X 0x%X < 0x%X > \n", op, hexA, hexB, naur);
+                printf("%s 0x%X 0x%X: 0x%X  \n", op, hexA, hexB, naur);
                         
             }
         }
@@ -85,5 +95,3 @@ int main(){
     fclose(commands);
     return 0;
 }
-
-//|| fscanf(commands, "%s %X", op, &hexA) == 2 || fscanf(commands, "%s %X %d", op, &hexA, &shift) == 3
